f8539202d0c10776dfedcde22b69ae30daa38b63
Tost000 <94283039+Tost000@users.noreply.github.com>
2024-06-14 19:46:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -56,3 +56,53 @@ if (window.location.pathname.endsWith("product.html")) {
 if (window.location.pathname.endsWith("cart.html")) {
   window.onload = loadCart;
 }
+const plus = document.getElementById("counter");
+const minus = document.getElementById("counter2");
+const prise = document.getElementById("product-price");
+const value = document.getElementById("value");
+const gb256 = document.getElementById("256");
+const gb512 = document.getElementById("512");
+
+function increment() {
+  let result = Number(value.textContent) + 1;
+  value.textContent = result;
+  if (gb512.classList.contains("active")) {
+    var priseIphone = Number(prise.textContent) + 1700;
+  } else {
+    var priseIphone = Number(prise.textContent) + 1500;
+  }
+  prise.textContent = priseIphone;
+}
+
+function decrement() {
+  let result = Number(value.textContent) - 1;
+  value.textContent = result;
+  if (value.innerText == 0) {
+    increment();
+  }
+  if (gb512.classList.contains("active")) {
+    var priseIphone = Number(prise.textContent) - 1700;
+  } else {
+    var priseIphone = Number(prise.textContent) - 1500;
+  }
+  prise.textContent = priseIphone;
+}
+
+function gb256Size() {
+  gb512.classList.remove("active");
+  gb256.classList.add("active");
+  prise.textContent = 1500;
+  value.textContent = 1;
+}
+
+function gb512Size() {
+  gb256.classList.remove("active");
+  gb512.classList.add("active");
+  prise.textContent = 1700;
+  value.textContent = 1;
+}
+
+gb256.addEventListener("click", gb256Size);
+gb512.addEventListener("click", gb512Size);
+plus.addEventListener("click", increment);
+minus.addEventListener("click", decrement);
